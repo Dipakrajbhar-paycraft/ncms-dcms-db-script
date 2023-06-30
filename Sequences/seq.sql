568cd0c98ea3a69f0987f6ee4e74cbec46c98b67
Dipakrajbhar <97012307+Dipakrajbhar-paycraft@users.noreply.github.com>
2023-06-30 11:40:40 +0530

    Add files via upload

---
+++ Sequences/seq.sql
@@ -0,0 +1,1090 @@
+-- inventory_blank_card_manager_image_code_seq1 definition
+
+-- DROP SEQUENCE inventory_blank_card_manager_image_code_seq1;
+
+CREATE SEQUENCE inventory_blank_card_manager_image_code_seq1
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- inventory_blank_card_seq definition
+
+-- DROP SEQUENCE inventory_blank_card_seq;
+
+CREATE SEQUENCE inventory_blank_card_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- inventory_id_seq definition
+
+-- DROP SEQUENCE inventory_id_seq;
+
+CREATE SEQUENCE inventory_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- issuer_id_seq definition
+
+-- DROP SEQUENCE issuer_id_seq;
+
+CREATE SEQUENCE issuer_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- load_seq definition
+
+-- DROP SEQUENCE load_seq;
+
+CREATE SEQUENCE load_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- master_seq definition
+
+-- DROP SEQUENCE master_seq;
+
+CREATE SEQUENCE master_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mcc_grp_id_seq definition
+
+-- DROP SEQUENCE mcc_grp_id_seq;
+
+CREATE SEQUENCE mcc_grp_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mkchr_bin_profile_image_code_seq definition
+
+-- DROP SEQUENCE mkchr_bin_profile_image_code_seq;
+
+CREATE SEQUENCE mkchr_bin_profile_image_code_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mkchr_bin_profile_image_code_seq1 definition
+
+-- DROP SEQUENCE mkchr_bin_profile_image_code_seq1;
+
+CREATE SEQUENCE mkchr_bin_profile_image_code_seq1
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mkchr_virtual_to_phy_id_seq definition
+
+-- DROP SEQUENCE mkchr_virtual_to_phy_id_seq;
+
+CREATE SEQUENCE mkchr_virtual_to_phy_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mo_disp_batch_seq definition
+
+-- DROP SEQUENCE mo_disp_batch_seq;
+
+CREATE SEQUENCE mo_disp_batch_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mo_dispatch_file_log_seq definition
+
+-- DROP SEQUENCE mo_dispatch_file_log_seq;
+
+CREATE SEQUENCE mo_dispatch_file_log_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- mo_limit_mngt definition
+
+-- DROP SEQUENCE mo_limit_mngt;
+
+CREATE SEQUENCE mo_limit_mngt
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- otp_id_seq definition
+
+-- DROP SEQUENCE otp_id_seq;
+
+CREATE SEQUENCE otp_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- outlet_id_seq definition
+
+-- DROP SEQUENCE outlet_id_seq;
+
+CREATE SEQUENCE outlet_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- outlet_seq_id definition
+
+-- DROP SEQUENCE outlet_seq_id;
+
+CREATE SEQUENCE outlet_seq_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- page_master_id_seq definition
+
+-- DROP SEQUENCE page_master_id_seq;
+
+CREATE SEQUENCE page_master_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- partner_id_seq definition
+
+-- DROP SEQUENCE partner_id_seq;
+
+CREATE SEQUENCE partner_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- password_policy_id_seq definition
+
+-- DROP SEQUENCE password_policy_id_seq;
+
+CREATE SEQUENCE password_policy_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- prd_bin_profile_image_code_seq definition
+
+-- DROP SEQUENCE prd_bin_profile_image_code_seq;
+
+CREATE SEQUENCE prd_bin_profile_image_code_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- prd_bin_template_image_code_seq definition
+
+-- DROP SEQUENCE prd_bin_template_image_code_seq;
+
+CREATE SEQUENCE prd_bin_template_image_code_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- prd_card_image_master_image_code_seq definition
+
+-- DROP SEQUENCE prd_card_image_master_image_code_seq;
+
+CREATE SEQUENCE prd_card_image_master_image_code_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- prd_card_image_master_seq definition
+
+-- DROP SEQUENCE prd_card_image_master_seq;
+
+CREATE SEQUENCE prd_card_image_master_seq
+	INCREMENT BY 1
+	MINVALUE 10
+	MAXVALUE 9999
+	START 26
+	CACHE 1
+	NO CYCLE;
+
+
+-- prd_txn_limit_template_seq definition
+
+-- DROP SEQUENCE prd_txn_limit_template_seq;
+
+CREATE SEQUENCE prd_txn_limit_template_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- product_id_seq definition
+
+-- DROP SEQUENCE product_id_seq;
+
+CREATE SEQUENCE product_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- pswd_history_id definition
+
+-- DROP SEQUENCE pswd_history_id;
+
+CREATE SEQUENCE pswd_history_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- queue_summary_seq definition
+
+-- DROP SEQUENCE queue_summary_seq;
+
+CREATE SEQUENCE queue_summary_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- raw_sequence definition
+
+-- DROP SEQUENCE raw_sequence;
+
+CREATE SEQUENCE raw_sequence
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- rej_dispatch_seq definition
+
+-- DROP SEQUENCE rej_dispatch_seq;
+
+CREATE SEQUENCE rej_dispatch_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- rej_mo_disp_seq definition
+
+-- DROP SEQUENCE rej_mo_disp_seq;
+
+CREATE SEQUENCE rej_mo_disp_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- rejection_seq definition
+
+-- DROP SEQUENCE rejection_seq;
+
+CREATE SEQUENCE rejection_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 999999999999999
+	START 74
+	CACHE 1
+	NO CYCLE;
+
+
+-- request_id_seq definition
+
+-- DROP SEQUENCE request_id_seq;
+
+CREATE SEQUENCE request_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- role_id_seq definition
+
+-- DROP SEQUENCE role_id_seq;
+
+CREATE SEQUENCE role_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- role_page_link_id_seq definition
+
+-- DROP SEQUENCE role_page_link_id_seq;
+
+CREATE SEQUENCE role_page_link_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_acc_details definition
+
+-- DROP SEQUENCE seq_acc_details;
+
+CREATE SEQUENCE seq_acc_details
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_acc_dr_cr_entry definition
+
+-- DROP SEQUENCE seq_acc_dr_cr_entry;
+
+CREATE SEQUENCE seq_acc_dr_cr_entry
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_acc_entry definition
+
+-- DROP SEQUENCE seq_acc_entry;
+
+CREATE SEQUENCE seq_acc_entry
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_acc_narr_details definition
+
+-- DROP SEQUENCE seq_acc_narr_details;
+
+CREATE SEQUENCE seq_acc_narr_details
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_acc_txn_src_mode definition
+
+-- DROP SEQUENCE seq_acc_txn_src_mode;
+
+CREATE SEQUENCE seq_acc_txn_src_mode
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_adj_ref_number definition
+
+-- DROP SEQUENCE seq_adj_ref_number;
+
+CREATE SEQUENCE seq_adj_ref_number
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_adj_ref_rrn definition
+
+-- DROP SEQUENCE seq_adj_ref_rrn;
+
+CREATE SEQUENCE seq_adj_ref_rrn
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_adj_summary_id definition
+
+-- DROP SEQUENCE seq_adj_summary_id;
+
+CREATE SEQUENCE seq_adj_summary_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_card_details_his_id definition
+
+-- DROP SEQUENCE seq_card_details_his_id;
+
+CREATE SEQUENCE seq_card_details_his_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_card_fee_summary_id definition
+
+-- DROP SEQUENCE seq_card_fee_summary_id;
+
+CREATE SEQUENCE seq_card_fee_summary_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_card_status_history definition
+
+-- DROP SEQUENCE seq_card_status_history;
+
+CREATE SEQUENCE seq_card_status_history
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_card_status_summary definition
+
+-- DROP SEQUENCE seq_card_status_summary;
+
+CREATE SEQUENCE seq_card_status_summary
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_card_status_summary_id definition
+
+-- DROP SEQUENCE seq_card_status_summary_id;
+
+CREATE SEQUENCE seq_card_status_summary_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_card_status_temp definition
+
+-- DROP SEQUENCE seq_card_status_temp;
+
+CREATE SEQUENCE seq_card_status_temp
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_corporate_id definition
+
+-- DROP SEQUENCE seq_corporate_id;
+
+CREATE SEQUENCE seq_corporate_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_country_white_summary_id definition
+
+-- DROP SEQUENCE seq_country_white_summary_id;
+
+CREATE SEQUENCE seq_country_white_summary_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_cust_his_id definition
+
+-- DROP SEQUENCE seq_cust_his_id;
+
+CREATE SEQUENCE seq_cust_his_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_emd_enq definition
+
+-- DROP SEQUENCE seq_emd_enq;
+
+CREATE SEQUENCE seq_emd_enq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_emp_onboard_summary definition
+
+-- DROP SEQUENCE seq_emp_onboard_summary;
+
+CREATE SEQUENCE seq_emp_onboard_summary
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_emp_salary_summary definition
+
+-- DROP SEQUENCE seq_emp_salary_summary;
+
+CREATE SEQUENCE seq_emp_salary_summary
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_fee_waiver_id definition
+
+-- DROP SEQUENCE seq_fee_waiver_id;
+
+CREATE SEQUENCE seq_fee_waiver_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_mo_limit_mngt definition
+
+-- DROP SEQUENCE seq_mo_limit_mngt;
+
+CREATE SEQUENCE seq_mo_limit_mngt
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_replaced_card definition
+
+-- DROP SEQUENCE seq_replaced_card;
+
+CREATE SEQUENCE seq_replaced_card
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_status definition
+
+-- DROP SEQUENCE seq_status;
+
+CREATE SEQUENCE seq_status
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_trickle_feed definition
+
+-- DROP SEQUENCE seq_trickle_feed;
+
+CREATE SEQUENCE seq_trickle_feed
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_unique_txn_id definition
+
+-- DROP SEQUENCE seq_unique_txn_id;
+
+CREATE SEQUENCE seq_unique_txn_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- seq_wallet_master_temp_id definition
+
+-- DROP SEQUENCE seq_wallet_master_temp_id;
+
+CREATE SEQUENCE seq_wallet_master_temp_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- seq_wallet_profile_id definition
+
+-- DROP SEQUENCE seq_wallet_profile_id;
+
+CREATE SEQUENCE seq_wallet_profile_id
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- serial_no_seq definition
+
+-- DROP SEQUENCE serial_no_seq;
+
+CREATE SEQUENCE serial_no_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- sftp_ud_id_seq definition
+
+-- DROP SEQUENCE sftp_ud_id_seq;
+
+CREATE SEQUENCE sftp_ud_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- state_id_seq definition
+
+-- DROP SEQUENCE state_id_seq;
+
+CREATE SEQUENCE state_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- status_history_id_seq definition
+
+-- DROP SEQUENCE status_history_id_seq;
+
+CREATE SEQUENCE status_history_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- status_update_id_seq definition
+
+-- DROP SEQUENCE status_update_id_seq;
+
+CREATE SEQUENCE status_update_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- tax_master_id_seq definition
+
+-- DROP SEQUENCE tax_master_id_seq;
+
+CREATE SEQUENCE tax_master_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 99999999999999
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- temp_cust_seq definition
+
+-- DROP SEQUENCE temp_cust_seq;
+
+CREATE SEQUENCE temp_cust_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- temp_mo_disp_seq definition
+
+-- DROP SEQUENCE temp_mo_disp_seq;
+
+CREATE SEQUENCE temp_mo_disp_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- topup_profile_id_seq definition
+
+-- DROP SEQUENCE topup_profile_id_seq;
+
+CREATE SEQUENCE topup_profile_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- txn_profile_id_seq definition
+
+-- DROP SEQUENCE txn_profile_id_seq;
+
+CREATE SEQUENCE txn_profile_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- txn_profile_id_templ_seq definition
+
+-- DROP SEQUENCE txn_profile_id_templ_seq;
+
+CREATE SEQUENCE txn_profile_id_templ_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- txn_ref_no_seq definition
+
+-- DROP SEQUENCE txn_ref_no_seq;
+
+CREATE SEQUENCE txn_ref_no_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 386438
+	CACHE 1
+	NO CYCLE;
+
+
+-- txn_type_id_seq definition
+
+-- DROP SEQUENCE txn_type_id_seq;
+
+CREATE SEQUENCE txn_type_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 100
+	CACHE 1
+	NO CYCLE;
+
+
+-- txnid_seq definition
+
+-- DROP SEQUENCE txnid_seq;
+
+CREATE SEQUENCE txnid_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9999999999999
+	START 1
+	CACHE 1
+	CYCLE;
+
+
+-- usage_profile_id_seq definition
+
+-- DROP SEQUENCE usage_profile_id_seq;
+
+CREATE SEQUENCE usage_profile_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- user_group_id_seq definition
+
+-- DROP SEQUENCE user_group_id_seq;
+
+CREATE SEQUENCE user_group_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- user_id_seq definition
+
+-- DROP SEQUENCE user_id_seq;
+
+CREATE SEQUENCE user_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- virtual_to_physical_seq definition
+
+-- DROP SEQUENCE virtual_to_physical_seq;
+
+CREATE SEQUENCE virtual_to_physical_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- virtualtophysical definition
+
+-- DROP SEQUENCE virtualtophysical;
+
+CREATE SEQUENCE virtualtophysical
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- wallet_id_seq definition
+
+-- DROP SEQUENCE wallet_id_seq;
+
+CREATE SEQUENCE wallet_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- wallet_profile_id_seq definition
+
+-- DROP SEQUENCE wallet_profile_id_seq;
+
+CREATE SEQUENCE wallet_profile_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 2147483647
+	START 1
+	CACHE 1
+	NO CYCLE;
+
+
+-- zone_id_seq definition
+
+-- DROP SEQUENCE zone_id_seq;
+
+CREATE SEQUENCE zone_id_seq
+	INCREMENT BY 1
+	MINVALUE 1
+	MAXVALUE 9223372036854775807
+	START 1
+	CACHE 1
+	NO CYCLE;
